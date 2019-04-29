f06e5ecd7eba92d91d3b28a05b081019ebb0d1e8
Fuzzy Hair <pamelasanchezhernandez@Pamelas-MBP.lan1>
2019-04-28 23:13:33 -0400

    updated schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers(burger_name,devoured)
+
+VALUES 
+("Cheeseburger",false),
+("Double Cheeseburger",false),
+("Kobe Beef Burger",false),
+("BLT Burger",false),
+("Bacon BBQ Burger",false),
+("Mushroom Swiss Burger",false),
+("Hamburger",false),
+("Veggie Burger",false),
+("Grass Fed Beef Burger with Cauliflower Bun",false);
